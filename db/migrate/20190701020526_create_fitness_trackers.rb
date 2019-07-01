ddb86b4d349ba24b458469e49060fb6e7880cfa1
addisonpohl <addison.c.pohl@gmail.com>
2019-06-30 21:25:41 -0500

    Add FitnessTracker Database

--- db/migrate/20190701020526_create_fitness_trackers.rb
+++ db/migrate/20190701020526_create_fitness_trackers.rb
@@ -1,10 +1,8 @@
 class CreateFitnessTrackers < ActiveRecord::Migration[5.2]
   def change
     create_table :fitness_trackers do |t|
-      t.string :weight
-      t.string :integer
-      t.string :bpm
-      t.string :integer
+      t.integer :weight
+      t.integer :bpm
       t.string :mood
 
       t.timestamps
